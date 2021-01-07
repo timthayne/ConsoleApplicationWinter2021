f9ac0b352aebb10ce3a5dbc5f092fc4b3f9693e2
Tim Thayne <timthayne@gmail.com>
2021-01-07 09:24:39 -0700

    Add project files.

---
+++ ConsoleApplicationWinter2021/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationWinter2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2.
+            var name = "Brother Thayne";
+            var location = "Rexburg, ID";
+
+            // 3.
+            Console.WriteLine($"My name is {name}");
+            Console.WriteLine($"I am from {location}");
+
+            // 4.
+            var currentDate = DateTime.Now;
+            Console.WriteLine("The current date is " + currentDate.ToString("d"));
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
